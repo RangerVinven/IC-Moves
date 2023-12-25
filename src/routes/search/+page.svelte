fbe18c30a26b118b5d1801456cb98aa563276b9f
Daniel McPherson <daniel.mcpherson@live.co.uk>
2023-12-25 22:51:29 +0100

    Created properties

---
+++ src/routes/search/+page.svelte
@@ -0,0 +1,95 @@
+<script>
+    import Navbar from "$lib/components/Navbar.svelte";
+    import PropertyPreview from "$lib/components/PropertyPreview.svelte";
+    import FilterButton from "./FilterButton.svelte";
+</script>
+
+<Navbar page="Search" />
+
+<main>
+    <div id="Properties-And-Filters">
+        <div id="Filters">
+            <div id="Filters-Container">
+                <FilterButton>Sort By</FilterButton>
+                <FilterButton>Type</FilterButton>
+                <FilterButton>Bedrooms</FilterButton>
+                <FilterButton>Showers</FilterButton>
+                <FilterButton>Noise Level</FilterButton>
+            </div>
+
+            <button>Apply Filters</button>
+        </div>
+
+        <div id="Properties">
+            <PropertyPreview />
+            <PropertyPreview />
+            <PropertyPreview />
+            <PropertyPreview />
+            <PropertyPreview />
+            <PropertyPreview />
+            <PropertyPreview />
+        </div>
+    </div>
+</main>
+
+<style>
+    #Properties-And-Filters {
+        margin-top: 15px;
+        margin-left: 10px;
+
+        display: flex;
+
+        align-items: flex-start;
+        justify-content: center;
+    }
+
+    #Filters {
+        width:15%;
+        height: 50dvh;
+
+        display: flex;
+        flex-direction: column;
+
+        justify-content: center;
+        align-items: center;
+    }
+
+    button {
+        cursor: pointer;
+        margin-top: 15px;
+
+        background-color: var(--dark_brown);
+        color: white;
+        
+        font-weight: bold;
+        font-size: 20px;
+
+        padding-top: 10px;
+        padding-bottom: 10px;
+        padding-right: 15px;
+        padding-left: 15px;
+
+        border: none;
+        border-radius: 6px;
+        width: fit-content;
+    }
+
+    #Filters-Container {
+        display: flex;
+        flex-direction: column;
+
+        justify-content: center;
+        align-items: center;
+
+        gap: 10px;
+    }
+
+    #Properties {
+        width: 80%;
+        height: 100dvh;
+
+        display: flex;
+        flex-wrap: wrap;
+        gap: 5%;
+    }
+</style>
---
+++ src/routes/search/FilterButton.svelte
@@ -0,0 +1,28 @@
+<button><h3><slot /></h3></button>
+
+<style>
+    button {
+        cursor: pointer;
+
+        width: 100%;
+        
+        padding-right: 15px;
+        padding-left: 15px;
+
+        background-color: var(--light_brown);
+        border: none;
+        border-radius: 5px;
+    }
+
+    h3 {
+        color: white;
+        font-size: 20px;
+
+        margin: 5%;
+        white-space: nowrap;
+    }
+
+    button:hover {
+        background-color: var(--dark_brown);
+    }
+</style>
