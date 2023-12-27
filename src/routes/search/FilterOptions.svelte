f7169ade022c11eb5086c56ebef1e12f6a88ff8c
Daniel McPherson <daniel.mcpherson@live.co.uk>
2023-12-27 22:52:52 +0000

    Created FilterOptions.svelte

---
+++ src/routes/search/FilterOptions.svelte
@@ -0,0 +1,45 @@
+<div id="Filters-Container">
+    <button>Sort By</button>
+    <button>Type</button>
+    <button>Bedrooms</button>
+    <button>Showers</button>
+    <button>Noise Level</button>
+</div>
+
+<style>
+    #Filters-Container {
+        display: flex;
+        flex-direction: column;
+
+        justify-content: center;
+        align-items: center;
+
+        gap: 10px;
+    }
+
+    button {
+        background-color: var(--light_brown);
+        border: none;
+
+        color: white;
+        font-size: 25px;
+
+        padding: 10px;
+
+        cursor: pointer;
+
+        border-radius: 5px;
+    }
+
+    @media only screen and (max-width: 900px) {
+        #Filters-Container {
+            flex-direction: row;
+            flex-wrap: wrap;
+
+            justify-content: center;
+            align-items: center;
+
+            gap: 10px;
+        }
+    }
+</style>
