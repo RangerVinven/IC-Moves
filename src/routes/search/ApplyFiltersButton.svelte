aebf09ffd5e9bdaab01fa9432b81f34ccea5223c
Daniel McPherson <daniel.mcpherson@live.co.uk>
2023-12-27 23:05:15 +0000

    Created ApplyFiltersButton

---
+++ src/routes/search/ApplyFiltersButton.svelte
@@ -0,0 +1,22 @@
+<button>Apply Filters</button>
+
+<style>
+    button {
+        cursor: pointer;
+        width: fit-content;
+
+        margin-top: 15px;
+
+        background-color: var(--dark_brown);
+        color: white;
+        
+        font-weight: bold;
+        font-size: 20px;
+
+        padding: 10px 15px 10px 15px;
+
+        border: none;
+        border-radius: 6px;
+    }
+</style>
+
