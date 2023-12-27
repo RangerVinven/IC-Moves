9ad76ff000aa0b27392e75f4e8abfba341ceb902
Daniel McPherson <daniel.mcpherson@live.co.uk>
2023-12-27 22:49:13 +0000

    Moved filters to MobileFilter

--- src/routes/search/+page.svelte
+++ src/routes/search/+page.svelte
@@ -1,7 +1,10 @@
 <script>
+  import MobileFilter from './MobileFilter.svelte';
+
     import Navbar from "$lib/components/Navbar.svelte";
     import PropertyPreview from "$lib/components/PropertyPreview.svelte";
-    import FilterButton from "./FilterButton.svelte";
+    
+    import FilterOptions from "./FilterOptions.svelte";
 </script>
 
 <Navbar page="Search" />
@@ -9,22 +12,20 @@
 <main>
     <div id="Properties-And-Filters">
         <div id="Filters">
-            <div id="Filters-Container">
-                <FilterButton>Sort By</FilterButton>
-                <FilterButton>Type</FilterButton>
-                <FilterButton>Bedrooms</FilterButton>
-                <FilterButton>Showers</FilterButton>
-                <FilterButton>Noise Level</FilterButton>
-            </div>
+            <FilterOptions />
 
             <button>Apply Filters</button>
         </div>
 
         <div id="Properties-And-Search">
+            <h1>Search</h1>
             <div id="Search-And-Button">
                 <input id="Search" placeholder="Enter a specific property" type="text">
                 <button>Search</button>
             </div>
+
+            <MobileFilter />
+
             <div id="Properties">
                 <PropertyPreview />
                 <PropertyPreview />
@@ -40,7 +41,9 @@
 </main>
 
 <style>
-
+    #Mobile-Filters {
+        display: none;
+    }
     #Properties-And-Filters {
         margin-top: 15px;
         margin-left: 10px;
@@ -62,7 +65,7 @@
         align-items: center;
     }
 
-    button {
+    button:not(#Filter-Arrow-Container) {
         cursor: pointer;
         margin-top: 15px;
 
@@ -82,14 +85,9 @@
         width: fit-content;
     }
 
-    #Filters-Container {
-        display: flex;
-        flex-direction: column;
-
-        justify-content: center;
-        align-items: center;
-
-        gap: 10px;
+    #Filter-Arrow-Container {
+        background-color: transparent;
+        border: none;
     }
 
     #Properties-And-Search {
@@ -121,5 +119,77 @@
 
     #Search-And-Button > button {
         margin-left: 15px;
+    }
+
+    h1 {
+        visibility: hidden;
+    }
+
+    @media only screen and (max-width: 900px) {
+        #Filters {
+            display: none;
+        }
+
+        #Properties {
+            flex-wrap: wrap;
+        }
+
+        h1 {
+            visibility: visible;
+            text-align: center;
+            font-size: 45px;
+
+            color: var(--light_brown);
+
+            margin: 0;
+        }
+
+        #Search-And-Button {
+            display: flex;
+            flex-direction: column;
+
+            align-items: center;
+            justify-content: center;
+
+            margin-top: 20px;
+        }
+
+        #Search {
+            width: 80%;
+        }
+
+        #Properties {
+            justify-content: center;
+        }
+
+        #Mobile-Filters {
+            display: block;
+            margin-bottom: 30px;
+        }
+
+        #Filter-Arrow-Container {
+            height: 30px;
+            width: 30px;
+            height: fit-content;
+        }
+
+        #Filters-And-Arrow-Container {
+            display: flex;
+            align-items: center;
+            gap: 10px;
+
+            width: fit-content;
+        }
+
+        #Filters-And-Arrow-Container > button {
+            cursor: pointer;
+        }
+
+        #Filters-And-Arrow-Container > h3 {
+            font-size: 35px;
+            font-family: Arial;
+
+            cursor: pointer;
+        }
     }
 </style>
