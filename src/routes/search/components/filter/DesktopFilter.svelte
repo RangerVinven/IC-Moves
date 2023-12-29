16d58ff47cef467dca11efaabe788023134693e4
Daniel McPherson <daniel.mcpherson@live.co.uk>
2023-12-29 15:55:53 +0000

    Moved to /filter directory

---
+++ src/routes/search/components/filter/DesktopFilter.svelte
@@ -0,0 +1,31 @@
+<script>
+import FilterButtons from './FilterButtons.svelte';
+import FilterOptions from "./FilterOptions.svelte";
+</script>
+
+<div id="Filters">
+    <FilterOptions />
+
+    <div style="display: flex;">
+        <FilterButtons />
+    </div>
+</div>
+
+<style>
+    #Filters {
+        width:15%;
+        height: 50dvh;
+
+        display: flex;
+        flex-direction: column;
+
+        justify-content: center;
+        align-items: center;
+    }
+
+    @media only screen and (max-width: 900px) {
+        #Filters {
+            display: none;
+        }
+    }
+</style>
---
+++ src/routes/search/components/filter/FilterButtons.svelte
@@ -0,0 +1,35 @@
+
+<div>
+    <button>Apply Filters</button>
+    <button id="Clear">Clear Filters</button>
+</div>
+
+<style>
+    button {
+        cursor: pointer;
+        width: fit-content;
+
+        margin-top: 15px;
+
+        background-color: var(--dark_brown);
+        color: white;
+        
+        font-weight: bold;
+        font-size: 20px;
+
+        padding: 10px 15px 10px 15px;
+
+        border: none;
+        border-radius: 6px;
+    }
+
+    #Clear {
+        background-color: transparent;
+        color: var(--dark_brown);
+
+        border: 1px solid;
+        border-color: var(--dark_brown);
+    }
+
+</style>
+
