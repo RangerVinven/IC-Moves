1908c62efc3cf0bd0f17a2f8caeea0456ddc7ab1
Daniel McPherson <daniel.mcpherson@live.co.uk>
2023-12-29 16:48:39 +0000

    Created PropertiesSearch

---
+++ src/routes/search/components/PropertiesSearch.svelte
@@ -0,0 +1,24 @@
+<script>
+    import PropertyPreview from "$lib/components/PropertyPreview.svelte";
+import SearchBar from "./SearchBar.svelte";
+
+</script>
+
+<div style="margin-left: 25px;">
+    <SearchBar />
+    
+    <div id="Properties">
+        {#each Array(10) as _}
+            <PropertyPreview />
+        {/each}
+    </div>
+</div>
+
+<style>
+    #Properties {
+        display: flex;
+        flex-wrap: wrap;
+
+        margin-top: 15px;
+    }
+</style>
