3ec3c48eb3738c9b71f754cb195d39e0b51f2cd5
Daniel McPherson <daniel.mcpherson@live.co.uk>
2023-12-29 16:45:28 +0000

    Rewrote the search page

--- src/routes/search/+page.svelte
+++ src/routes/search/+page.svelte
@@ -1,72 +1,42 @@
 <script>
 import Navbar from "$lib/components/Navbar.svelte";
-import SearchBar from './components/SearchBar.svelte';
-import PropertyPreview from "$lib/components/PropertyPreview.svelte";
 
 import DesktopFilter from "./components/filter/DesktopFilter.svelte";
-import MobileFilter from './components/filter/MobileFilter.svelte';
+import PropertiesSearch from './components/PropertiesSearch.svelte';
 </script>
 
 <Navbar page="Search" />
 
 <main>
-    <div id="Properties-And-Filters">
-        <DesktopFilter />
-
-        <div id="Properties-And-Search">
-            <SearchBar />
-
-            <div id="Mobile-Filter">
-                <MobileFilter />
-            </div>
-
-            <div id="Properties">
-                {#each {length: 7} as _, i}
-                    <PropertyPreview />
-                {/each}
-            </div>
+    <div id="Container">
+        <div id="Desktop-Filter">
+            <DesktopFilter />
         </div>
-       
+
+        <div id="Properties-Search">
+
+            <PropertiesSearch />
+        </div>
     </div>
+
 </main>
 
 <style>
-    #Mobile-Filter {
-        display: none;
-    }
-
-    #Properties-And-Filters {
-        margin-top: 15px;
-        margin-left: 10px;
-
+    #Container {
         display: flex;
 
-        align-items: flex-start;
         justify-content: center;
+
+        margin-top: 20px;
+        
     }
 
-    #Properties-And-Search {
+    #Desktop-Filter {
+        width: fit-content;
+    }
+
+    #Properties-Search {
         width: 80%;
     }
 
-    #Properties {
-        height: 100dvh;
-
-        display: flex;
-        flex-wrap: wrap;
-        gap: 5%;
-    }
-
-    @media only screen and (max-width: 900px) {
-        #Mobile-Filter {
-            display: block;
-            margin-bottom: 30px;
-        }
-
-        #Properties {
-            flex-wrap: wrap;
-            justify-content: center;
-
-        }
-    }
 </style>
