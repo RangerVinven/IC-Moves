3518f1a13fb02e846f450daa139d066ef60edda8
Daniel McPherson <daniel.mcpherson@live.co.uk>
2024-01-05 21:36:39 +0000

    Added the Property Title and Images elements

--- src/routes/properties/[property_id]/+page.svelte
+++ src/routes/properties/[property_id]/+page.svelte
@@ -1,7 +1,21 @@
 <script>
     import Navbar from "$lib/components/Navbar.svelte";
+    import PropertyTitle from './PropertyTitle.svelte';
+    import PropertyImages from "./PropertyImages.svelte";
 
     export let data;
 </script>
 
 <Navbar />
+
+<!-- Container Div -->
+<div style="display: flex;justify-content:center;">
+    <PropertyTitle />
+
+    <PropertyImages />
+
+</div>
+
+<style>
+
+</style>
