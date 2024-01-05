ea5d1ae454e6d827642fd29a05d1c79fa7619408
Daniel McPherson <daniel.mcpherson@live.co.uk>
2024-01-05 21:36:10 +0000

    Made the property title

---
+++ src/routes/properties/[property_id]/PropertyTitle.svelte
@@ -0,0 +1,30 @@
+<script>
+    import FaChevronLeft from 'svelte-icons/fa/FaChevronLeft.svelte'
+</script>
+
+<div style="display: flex;align-items:center;gap: 15px;">
+        <button id="Left-Arrow">
+            <FaChevronLeft />
+        </button>
+        <h1>19 Greenview Cottage - £295 PCM</h1>
+    </div>
+
+<style>
+    h1 {
+        font-weight: normal;
+        font-size: 50px;
+    }
+
+    #Left-Arrow {
+        height: 45px;
+        width: 40px;
+
+        color: var(--light_brown);
+
+        background-color: transparent;
+        border: none;
+
+        cursor: pointer;
+    }
+</style>
+
