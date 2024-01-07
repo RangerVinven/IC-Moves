cc30837c608b39dfb9ef45f6c7dc91c8b100b729
Daniel McPherson <daniel.mcpherson@live.co.uk>
2024-01-07 21:52:14 +0000

    Created HeartIcon

---
+++ src/routes/properties/[property_id]/HeartIcon.svelte
@@ -0,0 +1,15 @@
+<script lang="js">
+    import { IconHeart } from '@tabler/icons-svelte';
+    import { IconHeartFilled } from '@tabler/icons-svelte';
+
+    export let isFilled;
+    let iconSize = 32;
+</script>
+
+{#if isFilled} 
+    <div style="color: var(--light_brown);">
+        <IconHeartFilled size={iconSize} />
+    </div>
+{:else}
+    <IconHeart size={iconSize} />
+{/if}
