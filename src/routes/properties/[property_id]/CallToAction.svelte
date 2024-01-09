816d9c59e85ded8b44da288df01e59efe460e73f
Daniel McPherson <daniel.mcpherson@live.co.uk>
2024-01-09 11:24:51 +0000

    Created the call to action

---
+++ src/routes/properties/[property_id]/CallToAction.svelte
@@ -0,0 +1,37 @@
+<div id="Call-To-Action">
+    <button>Call The Realtor Now!</button>
+    <h3>Or call them at 07563 837164</h3>
+</div>
+
+<style>
+    #Call-To-Action {
+        display: flex;
+        align-items: center;
+        flex-direction: column;
+
+        margin-top: 20px;
+    }
+
+    button {
+        background-color: var(--light_brown);
+        border: none;
+        border-radius: 5px;
+
+        color: white;
+        font-weight: bold;
+
+        font-size: 30px;
+        padding: 10px;
+
+        cursor: pointer;
+    }
+
+    h3 {
+        font-weight: normal;
+        font-style: italic;
+        font-size: 25px;
+        font-family: Arial;
+
+        margin-top: 10px;
+    }
+</style>
