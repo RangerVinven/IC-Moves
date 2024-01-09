ff5fe709668f47fc416b88c664f6efac90ded85a
Daniel McPherson <daniel.mcpherson@live.co.uk>
2024-01-09 11:12:39 +0000

    Created MoreInfo.svelte

---
+++ src/routes/properties/[property_id]/MoreInfo.svelte
@@ -0,0 +1,31 @@
+<div id="More-Info">
+    <div>
+        <h3>Property Type - Flat</h3>
+        <h3>Rent - £295 PCM</h3>
+        <h3>Council Tax - £50</h3>
+        <h3>Furnished - Yes</h3>
+    </div>
+</div>
+
+<style>
+
+    #More-Info {
+        display: flex;
+        justify-content: center;
+    }
+
+    #More-Info div {
+        display: flex;
+        justify-content: center;
+        flex-wrap: wrap;
+
+        column-gap: 40px;
+        row-gap: 15px;
+    }
+
+    h3 {
+        font-family: Arial;
+        font-weight: normal;
+        font-size: 30px;
+    }
+</style>
