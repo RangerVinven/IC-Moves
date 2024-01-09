c8548138813d222f189ff0feb8d1469fc865b097
Daniel McPherson <daniel.mcpherson@live.co.uk>
2024-01-09 11:25:00 +0000

    Created the Map

---
+++ src/routes/properties/[property_id]/Map.svelte
@@ -0,0 +1,11 @@
+<div id="Map">
+    <iframe width="1000" height="450" title="Map showing the location of the property" src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d17293.186338927935!2d-3.845027397145044!3d57.19434575665224!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x4885f1596e0bdc61%3A0x33ddd11cd59b7203!2sAviemore!5e0!3m2!1sen!2suk!4v1704798963557!5m2!1sen!2suk" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
+</div>
+
+<style>
+    #Map {
+        display: flex;
+        justify-content: center;
+
+    }
+</style>
