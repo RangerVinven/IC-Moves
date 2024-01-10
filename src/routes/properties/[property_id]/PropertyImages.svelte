237831bea9500aef9847d13bb64785f521099ce8
Daniel McPherson <daniel.mcpherson@live.co.uk>
2024-01-10 22:46:01 +0000

    Made the scroll button dynamically sticky/absolute

--- src/routes/properties/[property_id]/PropertyImages.svelte
+++ src/routes/properties/[property_id]/PropertyImages.svelte
@@ -2,7 +2,18 @@
     import FaChevronDown from 'svelte-icons/fa/FaChevronDown.svelte'
 
     let otherImages; // The Other-Images div
+    let scrollButtonClass = "Scroll-Button-Absolute";
+
     let isScrollButtonHidden = false;
+
+    // Changes Scroll-Button to position absolute when Other-Images is scrolled to the top, position sticky otherwise
+    function changeClassIfScrolledToTop() {
+        if(otherImages.scrollTop === 0) {
+            scrollButtonClass = "Scroll-Button-Absolute"
+        } else {
+            scrollButtonClass = "Scroll-Button-Sticky"
+        }
+    }
 
     // Checks if the #Other-Images has been scrolled all the way
     function hasImagesScrolledToBottom() {
@@ -11,7 +22,11 @@
     }
 
     function hideScrollButton() {
-        isScrollButtonHidden = !isScrollButtonHidden;
+        isScrollButtonHidden = true;
+    }
+
+    function showScrollButton() {
+        isScrollButtonHidden = false;
     }
 
 </script>
@@ -20,7 +35,13 @@
     <img id="Main-Image" src="../assets/Property_Page/1.png" alt="A stone house">
     <div bind:this={otherImages} on:scroll={() => {
         let isScrolledToBottom = hasImagesScrolledToBottom();
-        if(isScrolledToBottom) hideScrollButton();
+
+        if(isScrolledToBottom) {
+            hideScrollButton();
+        } else {
+            changeClassIfScrolledToTop();
+            showScrollButton();
+        }
     }} id="Other-Images">
         <img src="../assets/Property_Page/2.png" alt="A stone house" />
         <img src="../assets/Property_Page/3.png" alt="A decorated living-room" />
@@ -28,7 +49,7 @@
         <img src="../assets/Property_Page/5.png" alt="An empty attic room" />
         
         {#if !isScrollButtonHidden}
-            <button id="Scroll-Button">
+            <button id="Scroll-Button" class={scrollButtonClass}>
                 <FaChevronDown />
             </button>
         {/if}
@@ -69,8 +90,6 @@
     }
 
     #Scroll-Button {
-        position: sticky;
-
         width: 100%;
         height: 50px;
 
@@ -80,4 +99,13 @@
         bottom: 0;
         cursor: pointer;
     }
+
+    .Scroll-Button-Absolute {
+        position: absolute;
+    }
+
+    .Scroll-Button-Sticky {
+        position: sticky;
+    }
+
 </style>
