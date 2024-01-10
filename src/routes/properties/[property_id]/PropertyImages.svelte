948c35d359bd9523ae5ab2786a0e3fb30e71e96d
Daniel McPherson <daniel.mcpherson@live.co.uk>
2024-01-10 22:15:37 +0000

    Added checks for if other-images is at the bottom

--- src/routes/properties/[property_id]/PropertyImages.svelte
+++ src/routes/properties/[property_id]/PropertyImages.svelte
@@ -1,14 +1,37 @@
 <script>
     import FaChevronDown from 'svelte-icons/fa/FaChevronDown.svelte'
+
+    let otherImages; // The Other-Images div
+    let isScrollButtonHidden = false;
+
+    // Checks if the #Other-Images has been scrolled all the way
+    function hasImagesScrolledToBottom() {
+        if(otherImages.scrollTop === (otherImages.scrollHeight - otherImages.offsetHeight)) return true;
+        return false;
+    }
+
+    function hideScrollButton() {
+        isScrollButtonHidden = !isScrollButtonHidden;
+    }
+
 </script>
 
 <div id="PropertyImages">
     <img id="Main-Image" src="../assets/Property_Page/1.png" alt="A stone house">
-    <div id="Other-Images">
+    <div bind:this={otherImages} on:scroll={() => {
+        let isScrolledToBottom = hasImagesScrolledToBottom();
+        if(isScrolledToBottom) hideScrollButton();
+    }} id="Other-Images">
         <img src="../assets/Property_Page/2.png" alt="A stone house" />
         <img src="../assets/Property_Page/3.png" alt="A decorated living-room" />
         <img src="../assets/Property_Page/4.png" alt="A nice kitchen" />
         <img src="../assets/Property_Page/5.png" alt="An empty attic room" />
+        
+        {#if !isScrollButtonHidden}
+            <button id="Scroll-Button">
+                <FaChevronDown />
+            </button>
+        {/if}
     </div>
 </div>
 
@@ -36,10 +59,25 @@
 
         height: 580px;
         overflow: scroll;
+
+        position: relative;
     }
 
     #Other-Images > img {
         width: 100%;
         height: auto;
+    }
+
+    #Scroll-Button {
+        position: sticky;
+
+        width: 100%;
+        height: 50px;
+
+        background-color: rgba(255, 255, 255, 0.75);
+        border: none;
+
+        bottom: 0;
+        cursor: pointer;
     }
 </style>
