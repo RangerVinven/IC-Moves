4e233b5fe5751d757ff4504fcc1be85ed375b485
Daniel McPherson <daniel.mcpherson@live.co.uk>
2024-01-18 21:08:27 +0000

    Made main image change if other image is clicked

--- src/routes/properties/[property_id]/PropertyImages.svelte
+++ src/routes/properties/[property_id]/PropertyImages.svelte
@@ -1,10 +1,18 @@
 <script>
     import OtherImages from './OtherImages/OtherImages.svelte';
+
+    function changeMainImage(imageToChangeTo) {
+        mainImage = imageToChangeTo;
+    }
+
+    let mainImage = "1";
+    let otherImages = {'1': 'A stone house', '2': 'A decorated living-room', '3': 'A landing with some rooms to the side', '4': 'A nice kitchen', '5': 'An empty attic'}
+
 </script>
 
 <div id="PropertyImages">
-    <img id="Main-Image" src="../assets/Property_Page/1.png" alt="A stone house">
-    <OtherImages />
+    <img id="Main-Image" src="../assets/Property_Page/{mainImage}.png" alt="A stone house">
+    <OtherImages {changeMainImage} {otherImages} />
 </div>
 
 <style>
@@ -22,6 +30,10 @@
     }
 
     @media screen and (max-width: 1200px) {
+        #Main-Image {
+            width: 100%;
+        }
+
         #PropertyImages {
             flex-direction: column;
             align-items: center;
