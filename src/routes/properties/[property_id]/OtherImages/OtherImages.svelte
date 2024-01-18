82a28684aa5785e539fea1bdb5d30f54bb6e92c4
Daniel McPherson <daniel.mcpherson@live.co.uk>
2024-01-18 21:05:59 +0000

    Adding images with each loop rather than hardcoding

--- src/routes/properties/[property_id]/OtherImages/OtherImages.svelte
+++ src/routes/properties/[property_id]/OtherImages/OtherImages.svelte
@@ -1,14 +1,17 @@
-<script>
+<script lang="js">
     import FaChevronDown from 'svelte-icons/fa/FaChevronDown.svelte'
 
-    let otherImages; // The Other-Images div
+    let otherImagesDiv; // The Other-Images div
     let scrollButtonClass = "Scroll-Button-Absolute";
 
     let isScrollButtonHidden = false;
 
+    export let changeMainImage; // Function to change the main image being displayed
+    export let otherImages;
+
     // Changes Scroll-Button to position absolute when Other-Images is scrolled to the top, position sticky otherwise
     function changeClassIfScrolledToTop() {
-        if(otherImages.scrollTop === 0) {
+        if(otherImagesDiv.scrollTop === 0) {
             scrollButtonClass = "Scroll-Button-Absolute"
         } else {
             scrollButtonClass = "Scroll-Button-Sticky"
@@ -17,7 +20,7 @@
 
     // Checks if the #Other-Images has been scrolled all the way
     function hasImagesScrolledToBottom() {
-        if(otherImages.scrollTop === (otherImages.scrollHeight - otherImages.offsetHeight)) return true;
+        if(otherImagesDiv.scrollTop === (otherImagesDiv.scrollHeight - otherImagesDiv.offsetHeight)) return true;
         return false;
     }
 
@@ -30,12 +33,12 @@
     }
 
     function scrollDown() {
-        otherImages.scrollBy(0, otherImages.clientHeight);
+        otherImagesDiv.scrollBy(0, otherImagesDiv.clientHeight);
     }
 
 </script>
 
-<div bind:this={otherImages} on:scroll={() => {
+<div bind:this={otherImagesDiv} on:scroll={() => {
     let isScrolledToBottom = hasImagesScrolledToBottom();
 
     if(isScrolledToBottom) {
@@ -45,16 +48,16 @@
         showScrollButton();
     }
 }} id="Other-Images">
-    <img src="../assets/Property_Page/2.png" alt="A stone house" />
-    <img src="../assets/Property_Page/3.png" alt="A decorated living-room" />
-    <img src="../assets/Property_Page/4.png" alt="A nice kitchen" />
-    <img src="../assets/Property_Page/5.png" alt="An empty attic room" />
+
+    {#each Object.entries(otherImages) as [image, altDescription]}
+        <button on:click={() => { changeMainImage(image) }} class="Other-Image-Button"><img src="../assets/Property_Page/{image}.png" alt={altDescription} /></button>
+    {/each}
     
     {#if !isScrollButtonHidden}
-        <button on:click={scrollDown} id="Scroll-Button" class={scrollButtonClass}>
-            <FaChevronDown />
-        </button>
-    {/if}
+    <button on:click={scrollDown} id="Scroll-Button" class={scrollButtonClass}>
+        <FaChevronDown />
+    </button>
+{/if}
 </div>
 
 <style>
@@ -75,9 +78,17 @@
         position: relative;
     }
 
-    #Other-Images > img {
+    #Other-Images > button > img {
         width: 100%;
         height: auto;
+    }
+
+    .Other-Image-Button {
+        border: none;
+        background-color: transparent;
+        cursor: pointer;
+
+        padding: none;
     }
 
     #Scroll-Button {
@@ -97,6 +108,28 @@
 
     .Scroll-Button-Sticky {
         position: sticky;
+    }
+
+    @media screen and (max-width: 1200px) {
+        #Other-Images {
+            flex-direction: row;
+        }
+
+        #Scroll-Button {
+            display: none;
+        }
+
+        #Other-Images {
+            margin-left: 0px;
+
+            width: 100vw;
+            height: 150px;
+        }
+
+        #Other-Images > img {
+            width: auto;
+            height: 100%;
+        }
     }
 
 </style>
