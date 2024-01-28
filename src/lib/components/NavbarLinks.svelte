52d0328faef64c1a7303f91a4437831826d0f6d1
Daniel McPherson <daniel.mcpherson@live.co.uk>
2024-01-28 22:23:23 +0000

    Added routing to /login

--- src/lib/components/NavbarLinks.svelte
+++ src/lib/components/NavbarLinks.svelte
@@ -4,7 +4,7 @@
 
 <li><a id={page === "Home" ? "active" : ""} href="/">Home</a></li>
 <li><a id={page === "Search" ? "active" : ""} href="/search">Search Properties</a></li>
-<li><a id={page === "Login" ? "active" : ""} href="/#">Login</a></li>
+<li><a id={page === "Login" ? "active" : ""} href="/login">Login</a></li>
 <li><a id={page === "Sign-Up" ? "active" : ""} href="/#">Sign-Up</a></li>
 
 <style>
