780d3a46b9d6c666252b30f2eff13509f8f4c7eb
Daniel McPherson <daniel.mcpherson@live.co.uk>
2024-01-29 13:59:49 +0000

    Added £ to the rent

--- src/routes/properties/[property_id]/MoreInfo.svelte
+++ src/routes/properties/[property_id]/MoreInfo.svelte
@@ -5,7 +5,7 @@
 <div id="More-Info">
     <div>
         <h3>Property Type - {data["property_type"]}</h3>
-        <h3>Rent - {data.rent} PCM</h3>
+        <h3>Rent - £{data.rent} PCM</h3>
         <h3>Council Tax - £{data["council_tax"]}</h3>
         <h3>Furnished - {data.furnished === false ? "No" : "Yes"}</h3>
     </div>
