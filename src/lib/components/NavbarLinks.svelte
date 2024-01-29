790057de2189f3f4bd5f14cdd1962b948bb546fc
Daniel McPherson <daniel.mcpherson@live.co.uk>
2024-01-29 15:10:49 +0000

    Added reading from store to see if user is logged in

--- src/lib/components/NavbarLinks.svelte
+++ src/lib/components/NavbarLinks.svelte
@@ -1,11 +1,24 @@
 <script lang="ts">
+    import SessionTokenStore from "../stores/SessionTokenStore";
     export let page: string;
+
+    let session_token = "";
+
+    SessionTokenStore.subscribe(data => {
+        session_token = data;
+    });
+    
 </script>
 
 <li><a id={page === "Home" ? "active" : ""} href="/">Home</a></li>
 <li><a id={page === "Search" ? "active" : ""} href="/search">Search Properties</a></li>
-<li><a id={page === "Login" ? "active" : ""} href="/login">Login</a></li>
-<li><a id={page === "Sign-Up" ? "active" : ""} href="/sign-up">Sign-Up</a></li>
+{#if session_token === ""}
+    <li><a id={page === "Login" ? "active" : ""} href="/login">Login</a></li>
+    <li><a id={page === "Sign-Up" ? "active" : ""} href="/sign-up">Sign-Up</a></li>
+{:else}
+    <li><a id={page === "My Account" ? "active" : ""} href="/myaccount">My Account</a></li>
+    <li><a id={page === "Sign-Out" ? "active" : ""} href="/signout">Sign-Out</a></li>
+{/if}
 
 <style>
     li {
