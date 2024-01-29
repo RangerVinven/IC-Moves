923c998d074f35073b91f8e50f2ec0b1ceaaced6
Daniel McPherson <daniel.mcpherson@live.co.uk>
2024-01-29 00:09:36 +0000

    Added login functionality

--- src/routes/login/+page.svelte
+++ src/routes/login/+page.svelte
@@ -1,30 +1,75 @@
 <script>
     import Navbar from "$lib/components/Navbar.svelte";
+
+    let email = "";
+    let password = "";
+
+    let incorrectCreds = false;
+
+    // Gets the session token if the credentials are correct
+    async function login(email, password) {
+        const res = await fetch("http://localhost:3000/login", {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json"
+            },
+            redirect: "follow",
+            body: JSON.stringify({
+                "email": email,
+                "password": password
+            })
+        })
+
+        // If the login details were correct
+        if(res.status === 200) {
+            const data = await res.json();
+
+            document.cookie = data["token"];
+            incorrectCreds = false;
+
+        // If the login details were wrong
+        } else if (res.status === 401) {
+            incorrectCreds = true;
+
+        // Something went wrong 
+        } else {
+            // Add something went wrong
+        }
+    }
+
 </script>
 
 <Navbar page="Login" />
 
-<div id="Login-Form-Container">
-    <div id="Form">
-        <h1>Login</h1>
+<div id="Mobile-Background">
+    <div id="Login-Form-Container">
+        <div id="Form">
+            <h1>Login</h1>
 
-        <div id="Inputs">
-            <div>
-                <h3>Email</h3>
-                <input type="text" />
+            <div id="Inputs">
+                <div>
+                    <h3>Email</h3>
+                    <input type="text" bind:value={email} />
+                </div>
+
+                <div>
+                    <h3>Password</h3>
+                    <input type="password" bind:value={password} />
+                </div>
             </div>
 
-            <div>
-                <h3>Password</h3>
-                <input type="password" />
+            <div id="Button">
+                <button on:click={() => login(email, password)}>Login</button>
             </div>
-        </div>
 
-        <div id="Button">
-            <button>Login</button>
-        </div>
+            {#if incorrectCreds}
+                <div id="Error-Message">
+                    <h6><i>Incorrect email or password</i></h6>
+                </div>
+            {/if}
 
-        <h6>Don't have an account? <b><a href="/sign-up">Sign-up</a></b></h6>
+            <h6>Don't have an account? <b><a href="/sign-up">Sign-up</a></b></h6>
+        </div>
     </div>
 </div>
 
@@ -129,4 +174,26 @@
         text-decoration: underline;
         color: white;
     }
+
+    @media only screen and (max-width: 600px) {
+        #Login-Form-Container {
+            height: 100dvh;
+            width: 100dvw;
+
+            overflow: hidden;
+        }
+
+        #Form {
+            height: 95dvh;
+        }
+
+        #Inputs {
+            margin-top: 15%;
+        }
+
+        #Mobile-Background {
+            background-color: var(--light_brown);
+        }
+    }
+
 </style>
