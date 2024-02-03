b833d61ee82f120d6e052f36da31a7bc5d39d62f
Daniel McPherson <daniel.mcpherson@live.co.uk>
2024-02-03 23:24:03 +0000

    Moved comments

--- src/routes/properties/[id]/+page.server.js
+++ src/routes/properties/[id]/+page.server.js
@@ -1,5 +1,6 @@
-// Gets the data about the specific property
 export async function load({ params }) {
+    
+    // Gets the data about the specific property
     const response = await fetch("http://127.0.0.1:8000/properties/" + params.id);
     const data = await response.json();
 
