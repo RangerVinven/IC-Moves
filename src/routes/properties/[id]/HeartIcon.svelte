c6706ca1d869a236a993d1a18851380f730da9f1
Daniel McPherson <daniel.mcpherson@live.co.uk>
2024-02-03 23:21:15 +0000

    Added loading icon

--- src/routes/properties/[id]/HeartIcon.svelte
+++ src/routes/properties/[id]/HeartIcon.svelte
@@ -1,19 +1,33 @@
 <script lang="js">
+    import { Circle } from "svelte-loading-spinners";
+    import { onMount } from "svelte";
+
     export let isFilled;
+    export let isSavedFunction;
+
     let iconSize = 32;
+    let hasLoaded = false;
+
+    onMount(() => {
+        isSavedFunction();
+        hasLoaded = true;
+    })
 </script>
 
-
-{#if isFilled}
-<!-- SVG from https://icons.getbootstrap.com/icons/heart-fill/ (altered slightly) -->
-<svg xmlns="http://www.w3.org/2000/svg" width={iconSize} height={iconSize} class="bi bi-heart-fill" viewBox="0 0 16 16">
-    <path id="Icon-Filled" fill-rule="evenodd" d="M8 1.314C12.438-3.248 23.534 4.735 8 15-7.534 4.736 3.562-3.248 8 1.314"/>
-  </svg>
+{#if hasLoaded}
+    {#if isFilled}
+        <!-- SVG from https://icons.getbootstrap.com/icons/heart-fill/ (altered slightly) -->
+        <svg xmlns="http://www.w3.org/2000/svg" width={iconSize} height={iconSize} class="bi bi-heart-fill" viewBox="0 0 16 16">
+            <path id="Icon-Filled" fill-rule="evenodd" d="M8 1.314C12.438-3.248 23.534 4.735 8 15-7.534 4.736 3.562-3.248 8 1.314"/>
+        </svg>
+    {:else}
+        <!-- SVG from https://icons.getbootstrap.com/icons/heart/ (altered slightly) -->
+        <svg xmlns="http://www.w3.org/2000/svg" width={iconSize} height={iconSize} class="bi bi-heart" viewBox="0 0 16 16">
+            <path d="m8 2.748-.717-.737C5.6.281 2.514.878 1.4 3.053c-.523 1.023-.641 2.5.314 4.385.92 1.815 2.834 3.989 6.286 6.357 3.452-2.368 5.365-4.542 6.286-6.357.955-1.886.838-3.362.314-4.385C13.486.878 10.4.28 8.717 2.01zM8 15C-7.333 4.868 3.279-3.04 7.824 1.143q.09.083.176.171a3 3 0 0 1 .176-.17C12.72-3.042 23.333 4.867 8 15"/>
+        </svg>
+    {/if}
 {:else}
-<!-- SVG from https://icons.getbootstrap.com/icons/heart/ (altered slightly) -->
-<svg xmlns="http://www.w3.org/2000/svg" width={iconSize} height={iconSize} class="bi bi-heart" viewBox="0 0 16 16">
-    <path d="m8 2.748-.717-.737C5.6.281 2.514.878 1.4 3.053c-.523 1.023-.641 2.5.314 4.385.92 1.815 2.834 3.989 6.286 6.357 3.452-2.368 5.365-4.542 6.286-6.357.955-1.886.838-3.362.314-4.385C13.486.878 10.4.28 8.717 2.01zM8 15C-7.333 4.868 3.279-3.04 7.824 1.143q.09.083.176.171a3 3 0 0 1 .176-.17C12.72-3.042 23.333 4.867 8 15"/>
-</svg>
+    <Circle color="var(--dark_brown)" size=30 />
 {/if}
 
 <style>
