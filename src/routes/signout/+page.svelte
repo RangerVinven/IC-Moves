9ac70770a38aadc562719ecfeb34a6f4a748bb1f
Daniel McPherson <daniel.mcpherson@live.co.uk>
2024-02-03 23:22:55 +0000

    Rewrote function inside onMount

--- src/routes/signout/+page.svelte
+++ src/routes/signout/+page.svelte
@@ -1,25 +1,20 @@
 <script>
     import { onMount } from "svelte";
+    import { goto } from "$app/navigation";
     import SessionTokenStore from "../../lib/stores/SessionTokenStore";
 
-    onMount(() => {
-
-        // Gets the session token from the store
-        let session_token = "";
-        SessionTokenStore.subscribe(data => {
-            session_token = data;
-        });
-
-        // Saves the session token to the cookies
-        document.cookie = "session_token=" + session_token
-
+    onMount(() => {        
         // Signs out
-        fetch("http://localhost:3000/signout").then(res => {
+        fetch("http://127.0.0.1:8000/logout", {
+            headers: {
+                "session_token": localStorage.getItem("session_token")
+            }
+        }).then(res => {
             if (res.status === 200) {
+                // Removes the session token
                 SessionTokenStore.set("")
-                document.cookie = "session_token=;"
-            } else {
-                console.log("Something went wrong");
+
+                goto("/");
             }
         })
     })
