1b1e39b2fc445203d2a173e7c5499394a754346d
Daniel McPherson <daniel.mcpherson@live.co.uk>
2024-02-05 22:01:34 +0000

    Made the Clear Saved button functional

--- src/routes/my-account/Properties.svelte
+++ src/routes/my-account/Properties.svelte
@@ -16,21 +16,46 @@
             const data = await response.json();
 
             properties = data;
-            console.log(properties);
         }
     })
+
+    async function clearSavedProperties() {
+        const res = await fetch("http://127.0.0.1:8000/saved/all", {
+            method: "DELETE",
+            headers: {
+                "session_token": localStorage.getItem("session_token")
+            }
+        });
+
+        if(res.status === 200) {
+            console.log("Properties deleted");
+            properties = [];
+        }
+    }
 </script>
 
 {#if !properties}
+
     <div id="Loading-Circle">
         <Circle color="var(--dark_brown)" size=80 />
     </div>
+
+{:else if properties.length === 0}
+
+    <h3>You don't have any properties saved.</h3>
+    
 {:else}
+
     <div id="Container">
         {#each properties as property}
             <PropertyPreview {property} />
         {/each}
+
+        <div id="Clear-Saved-Div">
+            <button on:click={clearSavedProperties}>Clear Saved</button>
+        </div>
     </div>
+
 {/if}
 
 <style>
@@ -45,5 +70,44 @@
         flex-wrap: wrap;
 
         width: 100%;
+    }
+
+    #Clear-Saved-Div {
+        display: flex;
+        justify-content: right;
+        margin-right: 15px;
+        
+
+        width: 100%;
+    }
+
+    button {
+        border: none;
+        border-radius: 5px;
+
+        font-size: 20px;
+        padding: 10px 20px;
+        margin-bottom: 15px;
+
+        background-color: var(--dark_brown);
+        color: white;
+
+        cursor: pointer;
+    }
+
+    button:hover {
+        background-color: var(--light_brown);
+    }
+
+    h3 {
+        font-size: 25px;
+        font-weight: normal;
+    }
+
+    @media only screen and (max-width: 600px) {
+        #Clear-Saved-Div {
+            margin-top: 20px;
+            justify-content: center;
+        }
     }
 </style>
