a1c673dea7faa1ac0d8ed795b8d0d2c09d79a964
Daniel McPherson <daniel.mcpherson@live.co.uk>
2024-02-07 18:59:41 +0000

    Created filterSearch()

--- src/routes/search/+page.svelte
+++ src/routes/search/+page.svelte
@@ -3,6 +3,37 @@ import Navbar from "$lib/components/Navbar.svelte";
 
 import DesktopFilter from "./components/filter/DesktopFilter.svelte";
 import PropertiesSearch from './components/PropertiesSearch.svelte';
+
+import FiltersStore from "$lib/stores/SearchFilters";
+import properties from "$lib/stores/PropertiesStore";
+import properties_empty from "$lib/stores/PropertiesEmptyStore";
+
+async function filterSearch() {
+    let parameters = "";
+
+    // Loops through the filters and adds them to the parameters (i.e, /search?name=x&min_showers=x)
+    for(const filter in $FiltersStore) {
+        if($FiltersStore[filter] !== "") {
+
+            // Doesn't add & to the start if it's the first parameter
+            if(parameters.length === 0) {
+                parameters = parameters + filter + "=" + $FiltersStore[filter];
+            } else {
+                parameters = parameters + "&" + filter + "=" + $FiltersStore[filter];
+            }
+        }
+    }
+    
+    const res = await fetch("http://127.0.0.1:8000/properties/search?" + parameters);
+    properties.set(await res.json());
+
+    if($properties.length === 0) {
+        properties_empty.set(true);
+    } else {
+        properties_empty.set(false);
+    }
+}
+
 </script>
 
 <Navbar page="Search" />
@@ -10,12 +41,12 @@ import PropertiesSearch from './components/PropertiesSearch.svelte';
 <main>
     <div id="Container">
         <div id="Desktop-Filter">
-            <DesktopFilter />
+            <DesktopFilter {filterSearch} />
         </div>
 
         <div id="Properties-Search">
 
-            <PropertiesSearch />
+            <PropertiesSearch {filterSearch} />
         </div>
     </div>
 
