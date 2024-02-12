5acdcac0755b20a78cca3d3cb7f4cfe5333f1254
Daniel McPherson <daniel.mcpherson@live.co.uk>
2024-02-12 22:43:04 +0000

    Added tick and cross

--- src/routes/my-account/+page.svelte
+++ src/routes/my-account/+page.svelte
@@ -62,12 +62,23 @@
 </div>
 
 <div id="Alert" class={alertStatus}>
+    {#if alertStatus === "Success"}
+        <!-- Taken from https://www.svgrepo.com/svg/43432/tick -->
+        <svg fill="white" height="24px" width="24px" version="1.1" id="Capa_1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" viewBox="0 0 490 490" xml:space="preserve" style="--darkreader-inline-fill: #000000;" data-darkreader-inline-fill=""><g id="SVGRepo_bgCarrier" stroke-width="0"></g><g id="SVGRepo_tracerCarrier" stroke-linecap="round" stroke-linejoin="round"></g><g id="SVGRepo_iconCarrier"> <polygon points="452.253,28.326 197.831,394.674 29.044,256.875 0,292.469 207.253,461.674 490,54.528 "></polygon> </g></svg>
+    {:else}
+        <!-- Taken from https://www.svgrepo.com/svg/80301/cross -->
+        <svg fill="white" height="24px" width="24px" version="1.1" id="Capa_1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" viewBox="0 0 490 490" xml:space="preserve" style="--darkreader-inline-fill: #000000;" data-darkreader-inline-fill=""><g id="SVGRepo_bgCarrier" stroke-width="0"></g><g id="SVGRepo_tracerCarrier" stroke-linecap="round" stroke-linejoin="round"></g><g id="SVGRepo_iconCarrier"> <polygon points="456.851,0 245,212.564 33.149,0 0.708,32.337 212.669,245.004 0.708,457.678 33.149,490 245,277.443 456.851,490 489.292,457.678 277.331,245.004 489.292,32.337 "></polygon> </g></svg>
+    {/if}
     { alertMessage }
 </div>
 
 <style>
 
     #Alert {
+        display: flex;
+        align-items: center;
+        gap: 10px;
+
         position: absolute;
         padding: 10px;
 
